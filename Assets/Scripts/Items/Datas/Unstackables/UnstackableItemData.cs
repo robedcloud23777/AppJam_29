33d484fb837f47cfbcce136d5b8165d9463cecf3
matthew0313 <85425949+matthew0313@users.noreply.github.com>
2025-02-08 15:29:27 +0900

    items test

--- Assets/Scripts/Items/Datas/Unstackables/UnstackableItemData.cs
+++ Assets/Scripts/Items/Datas/Unstackables/UnstackableItemData.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Unstackable Item Data", menuName = "Scriptables/Unstackables/Item", order = 0)]
 public class UnstackableItemData : ItemData
 {
     public override int maxStack => 1;
