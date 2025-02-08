f2ac5ac4455140c97f65193f49b501e6e66b14c9
matthew0313 <85425949+matthew0313@users.noreply.github.com>
2025-02-08 15:18:46 +0900

    dotween, inventory finish

--- Assets/Scripts/Entities/Player/Player.cs
+++ Assets/Scripts/Entities/Player/Player.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Player_Movement), typeof(Player_Animation))]
+[RequireComponent(typeof(Player_Movement), typeof(Player_Animation), typeof(Player_Inventory))]
 public class Player : MonoBehaviour
 {
     internal Player_Movement movement { get; private set; }
     new internal Player_Animation animation { get; private set; }
+    internal Player_Inventory inventory { get; private set; }
     private void Awake()
     {
         movement = GetComponent<Player_Movement>();
         animation = GetComponent<Player_Animation>();
+        inventory = GetComponent<Player_Inventory>();
         movement.OnAwake();
         animation.OnAwake();
+        inventory.OnAwake();
     }
     private void Update()
     {
-        
+        animation.OnUpdate();
     }
     private void FixedUpdate()
     {
---
+++ Assets/Scripts/Entities/Player/Player_Inventory.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Inventory : Inventory
+{
+    public void OnAwake()
+    {
+        Init();
+    }
+}
